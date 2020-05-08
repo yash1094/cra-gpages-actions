9844e8475db078e5714ede842bf34dc98961f0e6
Ajin Kabeer <ajiinkabeer@gmail.com>
2020-05-08 13:52:44 +0530

    Add routing and basic components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import React from "react";
+import { Route, Link } from "react-router-dom";
+import About from "./components/About";
+import Home from "./components/Home";
+import Contact from "./components/Contact";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hey you, yes you!</h1>
+      <h1>A basic react app</h1>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+      </ul>
+      <Route path="/" component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/contact" component={Contact} />
     </div>
   );
 }
