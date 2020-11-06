2ed71b320ef07e8032d1b50287cb77d9dadb8051
Yash Pandya <yash1094@gmail.com>
2020-11-06 15:11:38 -0500

    updated the home page component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,5 @@
 import React from "react";
 
-const Home = () => <h2>We have integrated GitHub Actions into our workflow</h2>;
+const Home = () => <h2>Welcome to the home page</h2>;
 
 export default Home;
